0cf79042a92fb940efced6bb8b7322f86b96b392
Mahamad Kamaruddin <104759072+mahamadkamaruddin@users.noreply.github.com>
2024-12-17 20:41:54 +0000

    regex match

--- Day-02/examples/03-regex-match.py
+++ Day-02/examples/03-regex-match.py
@@ -1,7 +1,17 @@
 import re
-
+#match will fail
 text = "The quick brown fox"
 pattern = r"quick"
+
+match = re.match(pattern, text)
+if match:
+    print("Match found:", match.group())
+else:
+    print("No match")
+
+#match will pass
+text = "The quick brown fox"
+pattern = r"The quick brown fox"
 
 match = re.match(pattern, text)
 if match:
