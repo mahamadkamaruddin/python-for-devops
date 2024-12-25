893167d1bb23f9ee661463eb2b29971eead1f2f0
mahamadkamaruddin <mohammadqamruddinkhateeb@gmail.com>
2024-12-25 22:26:51 +0530

    function definations and string

---
+++ Day-04/calculator-new.py
@@ -0,0 +1,15 @@
+num1=10
+num2=15
+
+def addition():
+    print(" the summation is : {}".format(num2+num1))
+
+def sub():
+    print("the substraction is : {}".format(abs(num1-num2)))
+
+def mul():
+    print("the multiplication is: {}".format(num1*num2))
+
+addition()
+sub()
+mul()
